01ad8cca495b7be812e333bcbb11c0fc4c413738
Emil Bajit <ebajit@gmail.com>
2024-11-30 17:19:16 -0500

    feat: Implemented slam_toolbox, updated urdf and mesh files, added test.world as the default gazebo world when launching in sim mode

--- src/ors_robot/launch/rsp_sim.launch.py
+++ src/ors_robot/launch/rsp_sim.launch.py
@@ -17,13 +17,26 @@ def generate_launch_description():
     file_subpath = 'urdf/robot.urdf.xacro'
 
 
-    # Add launch argument
+    # Add sim launch argument
     sim_arg = DeclareLaunchArgument(
         'sim', 
         default_value='false',
         description='Use simulated config (not using M2M2)',
     )
 
+    # Add gazebo world launch argument
+    world_arg = DeclareLaunchArgument(
+        'world', 
+        default_value='src/ors_robot/worlds/test.world',
+        description='Gazebo world file',
+    )
+
+    # Add slam_params_file arg for slam_toolbox
+    slam_params_file = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value='src/ors_robot/config/mapper_params_online_async.yaml',
+        description='Path to the ROS2 parameters file for the SLAM Toolbox',
+    )
 
     # Use xacro to process the file
     xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
@@ -47,6 +60,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={'world': LaunchConfiguration('world')}.items()
         )
 
 
@@ -54,6 +68,17 @@ def generate_launch_description():
         arguments=['-topic', 'robot_description',
                     '-entity', 'ors_robot'],
         output='screen')
+
+    # Launch slam_toolbox node
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch'), '/online_async_launch.py']),
+        launch_arguments={
+            'params_file': LaunchConfiguration('slam_params_file'),
+            'use_sim_time': 'true'
+        }.items(),
+        condition=IfCondition(LaunchConfiguration('sim'))
+    )
 
 
     slamtec_publisher = Node(
@@ -64,14 +89,18 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('sim')),
     )
 
+    # TODO: Add depth cam realsense node with launch condition
 
     # Run the node
     return LaunchDescription([
+        world_arg,
         gazebo,
         node_robot_state_publisher,
         spawn_entity,
         sim_arg,
         slamtec_publisher,
+        slam_params_file,
+        slam_toolbox,
         # RegisterEventHandler(
         #     OnProcessExit(
         #         target_action=slamtec_publisher,
