4791c7de1ef9dc2a5252187d43a6e9bb1655bd5b
sdstevenson <samirdstevenson@gmail.com>
2025-03-11 10:56:02 -0400

    fix: laser_frame from base_link instead of base_footprint

--- src/ldlidar_stl_ros2/launch/stl27l.launch.py
+++ src/ldlidar_stl_ros2/launch/stl27l.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
     package='tf2_ros',
     executable='static_transform_publisher',
     name='base_link_to_base_laser_stl27l',
-    arguments=['0','0','0.18','0','0','0','base_footprint','laser_frame']
+    arguments=['0','0','0.18','0','0','0','base_link','laser_frame']
   )
 
 
