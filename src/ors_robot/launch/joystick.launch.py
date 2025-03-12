57d43759da7306cf1482660a53d84f14ec9606af
sdstevenson <samirdstevenson@gmail.com>
2025-03-12 13:36:41 -0400

    fix: Add twist mux to joystick

--- src/ors_robot/launch/joystick.launch.py
+++ src/ors_robot/launch/joystick.launch.py
@@ -7,6 +7,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    pkg_name = 'ors_robot'
     use_sim_time = LaunchConfiguration('use_sim_time')
     namespace = LaunchConfiguration('namespace')
 
@@ -28,6 +29,7 @@ def generate_launch_description():
             package='joy',
             executable='joy_node',
             parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            namespace=namespace
         )
 
     teleop_node = Node(
@@ -38,6 +40,19 @@ def generate_launch_description():
             # remappings=[('/cmd_vel','/cmd_vel_joy')],
             namespace=namespace
         )
+
+    # Twist mux node
+    twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        name='twist_mux',
+        output='screen',
+        parameters=[{
+            'params_file': os.path.join(get_package_share_directory(pkg_name), 'config', 'twist_mux.yaml'),
+            'use_sim_time': {'use_sim_time': use_sim_time}
+        }],
+        namespace=namespace
+    )
 
     # twist_stamper = Node(
     #         package='twist_stamper',
@@ -51,6 +66,7 @@ def generate_launch_description():
     return LaunchDescription([
         use_sim_time_argument,
         namespace_argument,
+        twist_mux,
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
