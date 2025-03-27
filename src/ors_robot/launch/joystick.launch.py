b0b03e0d2191cd5b5662e85e4cdf8d5a57e237f0
ORS <Emil Bajit>
2025-03-27 09:51:25 -0400

    refactor: organize joystick

--- src/ors_robot/launch/joystick.launch.py
+++ src/ors_robot/launch/joystick.launch.py
@@ -12,33 +12,25 @@ def generate_launch_description():
     joy_params = os.path.join(get_package_share_directory('ors_robot'),'config','joystick.yaml')
 
     joy_node = Node(
-            package='joy',
-            executable='joy_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-        )
+        package='joy',
+        executable='joy_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}],
+    )
 
     teleop_node = Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            # remappings=[('/cmd_vel','/cmd_vel_joy')]
-        )
-
-    # twist_stamper = Node(
-    #         package='twist_stamper',
-    #         executable='twist_stamper',
-    #         parameters=[{'use_sim_time': use_sim_time}],
-    #         remappings=[('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
-    #                     ('/cmd_vel_out','/diff_cont/cmd_vel')]
-    #      )
-
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params, {'use_sim_time': use_sim_time}],
+        # remappings=[('/cmd_vel','/cmd_vel_joy')]
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
+            description='Use sim time if true'
+        ),
         joy_node,
         teleop_node,
         # twist_stamper       
